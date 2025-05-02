311936fd18510d61a3b028d2fb1268bdb76c5bc3
Matthieu <matthieu.chambon44@gmail.com>
2025-05-02 23:52:15 +0200

    Meilleure manière d'attribuer un href à chaque bouton du site

--- static/script.js
+++ static/script.js
@@ -1,16 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-  function addRedirectListener(buttonId) {
-    const btn = document.getElementById(buttonId);
-    if (btn) {
-      btn.addEventListener("click", function () {
-        window.location.href = btn.dataset.url;
-      });
-    }
-  }
+  const buttons = document.querySelectorAll("[data-url]");
 
-  addRedirectListener("back-to-login-btn");
-  addRedirectListener("create-ticket-btn");
-  addRedirectListener("edit-ticket-btn");
-  addRedirectListener("delete-ticket-btn");
-  addRedirectListener("ticket-cancel-btn");
+  buttons.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+      window.location.href = btn.dataset.url;
+    });
+  });
 });
